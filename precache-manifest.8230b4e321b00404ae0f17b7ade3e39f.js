38f77f1d6bfe110f5f1907e295101529fed7965b
Udot Andrii <udot.dron@gmail.com>
2019-11-26 22:06:07 +0200

    Updates

---
+++ precache-manifest.8230b4e321b00404ae0f17b7ade3e39f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a66b2549c25264ff182086b7d226f8",
+    "url": "/reactHW4UDOT/index.html"
+  },
+  {
+    "revision": "f23fb2e3666cc5fe53da",
+    "url": "/reactHW4UDOT/static/css/main.a9e39c5b.chunk.css"
+  },
+  {
+    "revision": "37d86d3d5ea89d3b73a1",
+    "url": "/reactHW4UDOT/static/js/2.d04b5b1b.chunk.js"
+  },
+  {
+    "revision": "f23fb2e3666cc5fe53da",
+    "url": "/reactHW4UDOT/static/js/main.2d4678a6.chunk.js"
+  },
+  {
+    "revision": "3e69d9c38b3559e3e1ed",
+    "url": "/reactHW4UDOT/static/js/runtime-main.861ce926.js"
+  },
+  {
+    "revision": "c81c378d0683db20c45486a4ac1750cd",
+    "url": "/reactHW4UDOT/static/media/Pause.c81c378d.png"
+  },
+  {
+    "revision": "1b874a35cfebe4efe4755fd9b801c3fc",
+    "url": "/reactHW4UDOT/static/media/play.1b874a35.png"
+  },
+  {
+    "revision": "dc971f8a14bba7a94d569d8672bf8793",
+    "url": "/reactHW4UDOT/static/media/snowflake.dc971f8a.png"
+  }
+]);
